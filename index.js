2ecf71cfefc8c518c884bdd9f31d105aa4cc3015
Artem Prygin <a.prygin@wellyes.ru>
2022-07-12 20:43:24 +0400

    change

--- index.js
+++ index.js
@@ -53,8 +53,13 @@
 //     // res.sendFile(indexPath);
 // });
 
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 5000
 
 express()
