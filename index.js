8eed94fecf131dfc5bf226b2c9f2ed408fdcfcbc
Artem Prygin <a.prygin@wellyes.ru>
2022-07-13 00:30:01 +0400

    changes

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ import express from 'express';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { WebSocketServer } from 'ws';
-import * as http from 'http';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const app = express();
-const server = http.createServer(app);
+const indexPath = path.join(__dirname, 'build/index.html');
 const PORT = process.env.PORT || 5000;
 
 const connectionHandler = (ws, msg) => {
@@ -24,7 +21,17 @@ const broadcastConnection = (msg) => {
     });
 };
 
-const indexPath = path.join(__dirname, 'build/index.html');
+const server = express()
+    .use(express.static('build'))
+    .get('/', (req, res) => {
+        res.sendFile(indexPath);
+    })
+    .get('/:id', (req, res) => {
+        res.sendFile(indexPath);
+    })
+    .listen(PORT, () => {
+        console.log(`Server started on PORT ${PORT}`);
+    });
 
 const wss = new WebSocketServer({ server });
 
@@ -43,17 +50,4 @@ wss.on('connection', (ws) => {
                 break;
         }
     });
-});
-
-app
-    .use(express.static('build'))
-    .get('/', (req, res) => {
-        res.sendFile(indexPath);
-    })
-    .get('/:id', (req, res) => {
-        res.sendFile(indexPath);
-    });
-
-server.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`);
 });
